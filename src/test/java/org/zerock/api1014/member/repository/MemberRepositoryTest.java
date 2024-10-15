cbce7fd349343e45bbcb759a00874a8e26887d83
Sangwon Lee <sang4939@gmail.com>
2024-10-15 09:52:22 +0900

    백업

--- src/test/java/org/zerock/api1014/member/repository/MemberRepositoryTest.java
+++ src/test/java/org/zerock/api1014/member/repository/MemberRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.api1014.cart.domain.Cart;
 import org.zerock.api1014.cart.domain.CartDetails;
+import org.zerock.api1014.cart.dto.CartDetailsListDTO;
 import org.zerock.api1014.cart.repository.CartDetailsRepository;
 import org.zerock.api1014.cart.repository.CartRepository;
 import org.zerock.api1014.common.dto.PageRequestDTO;
@@ -45,17 +46,49 @@ public class MemberRepositoryTest {
     public void testCartList(){
         String email = "user1@aaa.com";
 
-        Pageable pageable = PageRequest.of(0, 10);
+        Pageable pageable = PageRequest.of(0, 50);
 
-        cartDetailsRepository.listOfMember(email, pageable);
+        log.info(cartDetailsRepository.listOfMember(email, pageable).getContent());
     }
 
     @Test
     @Transactional
     @Commit
     public void testCartDummies(){
+        for (int i = 0; i < 10; i++) {
+            MemberEntity member = MemberEntity.builder()
+                    .email("user"+i+"@aaa.com")
+                    .pw(passwordEncoder.encode("1111"))
+                    .build();
+
+            Cart cart = Cart.builder()
+                    .member(member)
+                    .build();
+            cartRepository.save(cart);
+        }
 
     }
+
+    @Test
+    @Transactional
+    @Commit
+    public void testCartDetailDummies(){
+        for (int i = 1; i < 11; i++) {
+
+            CartDetails cartDetails = CartDetails.builder()
+                    .cart(Cart.builder()
+                            .cno((long) i)
+                            .build())
+                    .product(Product.builder()
+                            .pno((long) 2)
+                            .build())
+                    .qty(50)
+                    .build();
+            cartDetailsRepository.save(cartDetails);
+        }
+
+    }
+
 
     @Test
     @Transactional
@@ -75,3 +108,4 @@ public class MemberRepositoryTest {
         }
     }
 }
+
